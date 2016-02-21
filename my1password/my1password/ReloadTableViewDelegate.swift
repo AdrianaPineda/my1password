c7886859b6171eba70b6cc05ae592d30d7b7d533
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-21 16:21:01 -0500

    Add delegate class to refresh table view when adding account

---
+++ my1password/my1password/ReloadTableViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ReloadTableViewDelegate.swift
+//  my1password
+//
+//  Created by Adriana Pineda on 2/21/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ReloadTableViewDelegate: NSObject {
+
+}
