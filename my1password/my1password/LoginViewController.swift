41c086090682c7fdf59faced5cb8367274452f1e
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-25 17:17:45 -0500

    Set focus on password field in login view

--- my1password/my1password/LoginViewController.swift
+++ my1password/my1password/LoginViewController.swift
@@ -24,6 +24,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.password.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
