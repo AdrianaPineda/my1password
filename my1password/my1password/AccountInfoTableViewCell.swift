d7236b2b64dc00d7373d4790b46ff21af2cd0228
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-26 22:12:20 -0500

    Add custom cell class for account info view and assign it to password cell

---
+++ my1password/my1password/AccountInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountInfoTableViewCell.swift
+//  my1password
+//
+//  Created by Adriana Pineda on 3/26/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+
+class AccountInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
