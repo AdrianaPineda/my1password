d1ddad405797b4c6c5c039ac92a7a3d664e33d5c
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-12 21:57:18 -0500

    Start using use cases

---
+++ my1password/Reusable/UseCases/AccountsUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  AccountsUseCase.swift
+//  my1password
+//
+//  Created by Adriana Pineda on 10/12/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+
+class AccountsUseCase: NSObject {
+
+    func loadAccounts() -> [Account] {
+        return []
+    }
+
+}
