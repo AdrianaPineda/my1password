8c73c47bc61bfc888296d7772e5330692c1d1842
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-15 20:53:44 -0500

    Add User managed object class

---
+++ my1password/my1password/User+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  User+CoreDataProperties.swift
+//  my1password
+//
+//  Created by Adriana Pineda on 10/15/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension User {
+
+    @NSManaged var password: String?
+    @NSManaged var username: String?
+
+}
