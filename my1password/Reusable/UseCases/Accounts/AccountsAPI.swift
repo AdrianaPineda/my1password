bc1664d72d35a9cc71222214c5adf2b18a09cca7
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-29 19:12:41 -0500

    Add class to manage api calls to account endpoints

---
+++ my1password/Reusable/UseCases/Accounts/AccountsAPI.swift
@@ -0,0 +1,16 @@
+//
+//  AccountsAPI.swift
+//  my1password
+//
+//  Created by Adriana Pineda on 10/29/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+
+class AccountsAPI: NSObject {
+
+    func addAccount() -> (String) {
+        return "a"
+    }
+}
