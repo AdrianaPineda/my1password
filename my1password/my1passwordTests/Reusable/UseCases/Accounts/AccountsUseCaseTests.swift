90e1ef46162c2bc24a6d33b6da1df907ecbb59fb
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-29 20:35:29 -0500

    Remove spaces and rename test method

--- my1password/my1passwordTests/Reusable/UseCases/Accounts/AccountsUseCaseTests.swift
+++ my1password/my1passwordTests/Reusable/UseCases/Accounts/AccountsUseCaseTests.swift
@@ -33,38 +33,38 @@ class AccountsUseCaseTests: XCTestCase {
         }
     }
     
-    func testAddAccount() {
-        
+    func testAddAndLoadAccount() {
+
         let managedObjectContext = CoreDataHelpers().setUpInMemoryManagedObjectContext()
-        
+
         guard let entity = Account.entity(forManagedContext: managedObjectContext) else {
             XCTFail()
             return
         }
-        
+
         let username = "username_1"
         let password = "password_1"
         let url = "url_1"
-        
+
         let account = NSManagedObject(entity: entity, insertInto: managedObjectContext)
         account.setValue(username, forKey: "username")
         account.setValue(password, forKey: "password")
         account.setValue(url, forKey: "url")
-        
-        
+
+
         let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Account")
         do {
-            
+
             guard let accounts: [NSManagedObject] = try managedObjectContext.fetch(fetchRequest) as? [NSManagedObject] else {
                 XCTFail()
                 return
             }
-            
+
             XCTAssertTrue(accounts.count == 1)
             XCTAssertTrue(accounts[0].value(forKey: "username") as! String == username)
             XCTAssertTrue(accounts[0].value(forKey: "password") as! String == password)
             XCTAssertTrue(accounts[0].value(forKey: "url") as! String == url)
-        
+
         } catch {
             XCTFail()
         }
