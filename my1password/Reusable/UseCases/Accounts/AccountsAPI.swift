3e76eca41ea23d1221acd9cc0850c0101ab4618a
Adriana Pineda <apineda@twilio.com>
2017-01-04 21:47:02 -0500

    Add method to get accounts

--- my1password/Reusable/UseCases/Accounts/AccountsAPI.swift
+++ my1password/Reusable/UseCases/Accounts/AccountsAPI.swift
@@ -7,10 +7,35 @@
 //
 
 import UIKit
+import SwiftHTTP
 
 class AccountsAPI: NSObject {
 
-    func addAccount() -> (String) {
-        return "a"
+    fileprivate let apiServerUrl = "www.localhost.com"
+    fileprivate let accountsEndpoint = "%@/users/%@/accounts"
+
+    func getAccounts(forUser userId: String, handler: @escaping ((Response?) -> (Void))) {
+
+        let getAccountsUrl = String(format: accountsEndpoint, apiServerUrl, userId)
+
+        do {
+
+            let request = try HTTP.GET(getAccountsUrl)
+            request.start { response in
+
+                if let err = response.error {
+                    print("Error \(err.localizedDescription)")
+                    handler(nil)
+                    return
+                }
+
+                handler(response)
+
+            }
+
+        } catch {
+            handler(nil)
+            return
+        }
     }
 }
